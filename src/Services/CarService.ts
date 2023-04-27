08fa384cb658db6fb36fad0f0614e2aa4d0c274f
João <joaolelles7@gmail.com>
2023-04-27 20:02:35 -0300

    Requisito 1, Feat: Criando a rota /cars onde seja possível cadastrar um carro

---
+++ src/Services/CarService.ts
@@ -0,0 +1,17 @@
+import Car from '../Domains/Car';
+import ICar from '../Interfaces/ICar';
+import CarODM from '../Models/CarODM';
+
+export default class CarService {
+  private createCarDomain(car: ICar | undefined): Car | undefined {
+    if (car) {
+      return new Car(car);
+    }
+  }
+
+  public async createNewCar(car: ICar) {
+    const carODM = new CarODM();
+    const newCar = await carODM.create(car);
+    return this.createCarDomain(newCar);
+  }
+}
